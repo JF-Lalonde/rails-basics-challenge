eb2cd3880f8715557e988d2f623b7967894a621d
JF-Lalonde <lalondej88@gmail.com>
2017-07-10 14:43:41 -0600

    added name column to courses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710202929) do
+ActiveRecord::Schema.define(version: 20170710204146) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "street"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20170710202929) do
   create_table "courses", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "enrollments", force: :cascade do |t|
